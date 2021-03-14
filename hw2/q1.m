a4c5bbdc2462bf7db03f9d3901715482358c82eb
Nikita <nteplitskiy@TX150>
2021-03-14 03:18:03 -0400

    add LMMSE

--- hw2/q1.m
+++ hw2/q1.m
@@ -10,13 +10,22 @@ M = 1e6;
 % estimating Y from the observations X = Y + W. Verify that your simulation
 % is correct by comparing theoretical and empirical values of the MSE. 
 % Report your results in a table.
+%% Example 8.5 
 Y = 1 - 2*rand(1,M);
 W = 2 - 4*rand(1,M);
 X = Y + W;
-y = arrayfun(@(x)mmse1(x),X);
-MSE = mean((Y - y).^2);
+y = arrayfun(@(x)mmse1(x),X); % shouldn't need lambda wrap but do, why?
+fprintf("Expected MMSE: %f\n", 1/4);
+fprintf("Estimated MMSE: %f\n", mean((Y - y).^2));
 % This give 1/4 which is as expected
 % mean((Y - 0).^2) = 1/3 
+
+%% Example 8.6
+% Linear MMMSE 
+y = 1/5*X;
+fprintf("Expected MMSE: %f\n", 4/15);
+fprintf("Estimated MMSE: %f\n", mean((Y - y).^2));
+
 function yhat=mmse1(x)
     if (-3 <= x && x < -1) 
         yhat = 0.5 + .5 * x;
