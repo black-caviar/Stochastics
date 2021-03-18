27e638a5e7b4ad0362ce95d22513eb8872eae993
Nikita <nteplitskiy@TX150>
2021-03-17 21:04:01 -0400

    change title format

--- hw2/q1.m
+++ hw2/q1.m
@@ -1,17 +1,17 @@
 %% Bayes and Linear MMSE Estimators 
 % Nikita Teplitskiy, Dan Brody, and I-an Huang
+%% Scenario 1 
+% Implement the Bayes MMSE and Linear MMSE estimators from examples 
+% 8.5 and 8.6. Simulate this system by random draws of Y and W, and then 
+% estimating Y from the observations X = Y + W. Verify that your simulation
+% is correct by comparing theoretical and empiricalues of the MSE. 
+% Report your results in a table.
 clc
 clear all
 close all
 M = 1e6;
 %array to store expected and estimated MMSE and convert to table
 result_arr = zeros(2,3);
-%% Scenario 1 
-% Implement the Bayes MMSE and Linear MMSE estimators from examples 
-% 8.5 and 8.6. Simulate this system by random draws of Y and W, and then 
-% estimating Y from the observations X = Y + W. Verify that your simulation
-% is correct by comparing theoretical and empirical values of the MSE. 
-% Report your results in a table.
 %% Example 8.5 
 Y = 1 - 2*rand(1,M); % $ Y in [-1,1]$
 W = 2 - 4*rand(1,M); % $ W in [-2,2]$
@@ -34,7 +34,7 @@ result_arr(2,3) = mean((Y - y).^2);
 
 array2table(result_arr,'VariableNames', {'Example #','Expected MMSE','Estimated MMSE'})
 
-
+%%
 function yhat=mmse1(x)
     if (-3 <= x && x < -1) 
         yhat = 0.5 + .5 * x;
