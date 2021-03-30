cbc6d23d2f13fb82c5f97fa61e6bf6c8ba1b82cb
Nikita <nteplitskiy@TX150>
2021-03-29 21:54:15 -0400

    add estimators

--- hw3/q1.m
+++ hw3/q1.m
@@ -8,8 +8,34 @@
 % this for a couple of values of l.
 clearvars
 close all
-mmse = @(x1,x2) mean((x1-x2).^2);
-M = 1e3;
-%% 
-X_exp = exprnd(1, 1, M) % What should the mean be?
-X_ray = raylrnd(
+mse = @(x1,x2) (x1-x2).^2;
+M = 1e4;
+N = 5;
+%% For Exponential
+
+
+l = 4;
+mse_exp = zeros(1,N);
+for i = 1:N
+    X_exp = exprnd(1/l, M, i); % What should mu be?
+    l_hat = i/mean(sum(X_exp, 2));
+    mse_exp(i) = l_hat;
+    %mse_ray(i) = mmse(sqrt(s_hat), B);
+end
+mse_exp
+plot(1:N,mse_exp)
+
+%% For Rayleight 
+X_exp = exprnd(1, 1, M); % What should mu be?
+
+B = 2;
+mse_ray = zeros(1,N);
+for i = 1:N
+    X_ray = raylrnd(B, M, i); % What should B be?
+    s_hat = mean(sum(X_ray.^2, 2))/(2*i);
+    mse_ray(i) = sqrt(s_hat);
+    %mse_ray(i) = mmse(sqrt(s_hat), B);
+end
+mse_ray
+plot(1:N,mse_ray)
+
