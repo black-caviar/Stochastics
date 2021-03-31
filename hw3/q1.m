0a5c4672d65ab8ca438148f3977e0d6c9d0602e9
Nikita <nteplitskiy@TX150>
2021-03-31 01:39:16 -0400

    added comments, upsized graphs

--- hw3/q1.m
+++ hw3/q1.m
@@ -1,4 +1,6 @@
 %% Part 1
+% Names go here
+%%
 % Generate random draws from both of the exponential and 
 % Rayleigh random variables. You can use the EXPRND and RAYRND functions 
 % in MATLAB for this. Implement your ML estimators in MATLAB and plot 
@@ -10,6 +12,7 @@ clear all
 close all
 
 M = 1e4;
+N = 12;
 
 % ML estimator for exponential random variable
 % N: number of observations
@@ -21,33 +24,38 @@ exp_estimator = @(N, X) N./sum(X, 1);
 % X: N by M array of observations
 ray_estimator = @(N, X) sqrt(sum(X.^2, 1)./(2*N));
 
+% preallocate arrays 
 exp_MSE = zeros(10, 4);
 ray_MSE = zeros(10, 4);
 exp_bias = zeros(10, 4);
 ray_bias = zeros(10, 4);
 exp_var = zeros(10, 4);
 ray_var = zeros(10, 4);
+% Compute for 4 parameter values 
 for lambda=1:4
     alpha = lambda;
-    for N=1:10
-        X = exprnd(1/lambda, N, M);
-        lambda_hat = exp_estimator(N, X);
-        exp_MSE(N, lambda) = mean((lambda - lambda_hat).^2);
-        exp_bias(N, lambda) = mean(lambda - lambda_hat);
-        exp_var(N, lambda) = var(lambda_hat);
+    for n=1:N
+        % Compute statistics for exponential distribution
+        X = exprnd(1/lambda, n, M); % generate random variable 
+        lambda_hat = exp_estimator(n, X); % estimate parameter
+        exp_MSE(n, lambda) = mean((lambda - lambda_hat).^2); % compute MMSE
+        exp_bias(n, lambda) = mean(lambda - lambda_hat); % compute bias
+        exp_var(n, lambda) = var(lambda_hat); % compute variance 
         
-        X = raylrnd(alpha, N, M);
-        alpha_hat = ray_estimator(N, X);
-        ray_MSE(N, alpha) = mean((alpha - alpha_hat).^2);
-        ray_bias(N, alpha) = mean(alpha - alpha_hat);
-        ray_var(N, alpha) = var(lambda_hat);
+        % Compute statistics for Rayleigh distribution 
+        X = raylrnd(alpha, n, M);
+        alpha_hat = ray_estimator(n, X);
+        ray_MSE(n, alpha) = mean((alpha - alpha_hat).^2);
+        ray_bias(n, alpha) = mean(alpha - alpha_hat);
+        ray_var(n, alpha) = var(lambda_hat);
     end
 end
 
-% MSE of first 2 observations too large
+%%
+% MSE of first 2 observations too large to plot
 
 subplot(3, 2, 1)
-plot(3:10, exp_MSE(3:10,:))
+plot(3:N, exp_MSE(3:N,:))
 ylabel('Mean Squared Error')
 xlabel('# of Observations')
 title('MLE of exponential random variable');
@@ -56,7 +64,7 @@ legend({'$\lambda = 1$','$\lambda = 2$', ...
     'Interpreter','latex');
 
 subplot(3, 2, 2)
-plot(3:10, ray_MSE(3:10,:))
+plot(3:N, ray_MSE(3:N,:))
 ylabel('Mean Squared Error')
 xlabel('# of Observations')
 title('MLE of Rayleigh random variable');
@@ -65,7 +73,7 @@ legend({'$\alpha = 1$','$\alpha = 2$', ...
     'Interpreter','latex');
 
 subplot(3, 2, 3)
-plot(3:10, exp_bias(3:10,:))
+plot(3:N, exp_bias(3:N,:))
 ylabel('Bias')
 xlabel('# of Observations')
 title('MLE of exponential random variable');
@@ -74,7 +82,7 @@ legend({'$\lambda = 1$','$\lambda = 2$', ...
     'Interpreter','latex');
 
 subplot(3, 2, 4)
-plot(3:10, ray_bias(3:10,:))
+plot(3:N, ray_bias(3:N,:))
 ylabel('Bias')
 xlabel('# of Observations')
 title('MLE of Rayleigh random variable');
@@ -83,7 +91,7 @@ legend({'$\alpha = 1$','$\alpha = 2$', ...
     'Interpreter','latex');
 
 subplot(3, 2, 5)
-plot(3:10, exp_var(3:10,:))
+plot(3:N, exp_var(3:N,:))
 ylabel('Variance')
 xlabel('# of Observations')
 title('MLE of exponential random variable');
@@ -92,10 +100,12 @@ legend({'$\lambda = 1$','$\lambda = 2$', ...
     'Interpreter','latex');
 
 subplot(3, 2, 6)
-plot(3:10, ray_var(3:10,:))
+plot(3:N, ray_var(3:N,:))
 ylabel('Variance')
 xlabel('# of Observations')
 title('MLE of Rayleigh random variable');
 legend({'$\alpha = 1$','$\alpha = 2$', ...
     '$\alpha = 3$','$\alpha = 4$'}, ...
     'Interpreter','latex');
+
+set(gcf, 'Position', [0 0 1200 850])
