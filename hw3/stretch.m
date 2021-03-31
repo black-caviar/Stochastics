60d5444f2fa86484fcf4d3ac6847856c64a03096
Nikita <nteplitskiy@TX150>
2021-03-31 18:57:36 -0400

    do brute force estimation

--- hw3/stretch.m
+++ hw3/stretch.m
@@ -8,13 +8,16 @@ n = randi(100)
 l1 = randi(100)-1
 l2 = randi(100)-1
 
-y = [poissrnd(l1, 1, n), poissrnd(l2, 1, 100-n)]
+y = [poissrnd(l1, 1, n), poissrnd(l2, 1, 100-n)];
 
 histogram(y,20,'Normalization','pdf')
 hold on
 plot(0:100, poisspdf(0:100,l1))
 plot(0:100, poisspdf(0:100,l2))
 
-function y=pwasan(k, l)
-    
+est = zeros(101,2);
+lhat = @(x) sum(x)/length(x);
+
+for i=0:100
+    est(i+1,:) = [lhat(y(1:i)), lhat(y(i+1:end))];
 end
