5f39dbaf5d9886c1f687f0988a6fb92a3118c2a0
Nikita <nteplitskiy@TX150>
2021-04-20 02:16:37 -0400

    test results look good

--- hw4/q2.m
+++ hw4/q2.m
@@ -7,6 +7,7 @@ load('Iris.mat')
 % So I assume that there are 3 normally distributed 
 
 % Kind of elegant way to split but does not shuffle 
+% Shuffle really doesn't matter 
 split = false(1,length(features)); 
 split(randperm(length(features), length(features)/2)) = 1;
 train = features(split,:); 
@@ -21,7 +22,21 @@ for i = 1:3
     Sig(:,:,i) = cov(X_i);
 end
 
+% move this out to a function
 prb = zeros(3, length(train));
 for i = 1:3
     prb(i,:) = mvnpdf(train, mu(i,:), Sig(:,:,i));
 end
+
+[~,train_test] = max(prb)
+train_test == t_labels'
+
+%% Lets test classifier 
+prb = zeros(3, length(val));
+for i = 1:3
+    prb(i,:) = mvnpdf(val, mu(i,:), Sig(:,:,i));
+end
+
+[~,val_test] = max(prb)
+val_test == labels(~split)' 
+% it should majority correct 
